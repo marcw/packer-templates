a7a382cc3a649825ef51a1c30cf7fae99d0efc3a
Geoffrey Bachelet <grosfrais@gmail.com>
2013-07-07 20:13:25 -0400

    directly create a vagrant user instead of a packer user

--- web-php/scripts/apt.sh
+++ web-php/scripts/apt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-    cat <<EOC
+cat > /etc/apt/sources.list <<EOC
 deb http://packages.dotdeb.org wheezy all
 deb http://packages.dotdeb.org wheezy-php55 all
 
 deb http://ftp.ca.debian.org/debian stable main contrib non-free
 deb http://ftp.debian.org/debian/ wheezy-updates main contrib non-free
 deb http://security.debian.org/ wheezy/updates main contrib non-free
-EOC > /etc/apt/sources.list
-    curl -fsSL http://www.dotdeb.org/dotdeb.gpg | apt-key add -
-    apt-get update
+EOC
+
+#wget http://www.dotdeb.org/dotdeb.gpg -O - | apt-key add -
+#apt-get update

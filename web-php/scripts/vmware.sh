bbbcf8aaaedc89590a2dbab43c1a8ab57a445c54
Geoffrey Bachelet <grosfrais@gmail.com>
2013-07-13 10:33:47 -0400

    remove vmware directory when finished

--- web-php/scripts/vmware.sh
+++ web-php/scripts/vmware.sh
@@ -8,5 +8,6 @@ tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 /tmp/vmware-tools-distrib/vmware-install.pl -d
 
 umount /mnt/vmware
+rmdir /mnt/vmware
 rm -rf /tmp/vmware-tools-distrib/
 rm -f /tmp/linux.iso
